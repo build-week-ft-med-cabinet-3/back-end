13329517b195b509836682e1cf3a674dbdb2cc76
Gregory Wilson <gjwilson7390@gmail.com>
2020-09-22 12:39:06 -0500

    edit and delete user enabled

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -78,6 +78,25 @@ router.put('/:id', (req, res) => {
       res.status(500).json({ message: 'Failed to update user' });
     });
 });
+
+
+router.delete('/:id', (req, res) => {
+
+    const {id} = req.params
+
+    Users.remove(id)
+    .then(deleted => {
+        if (deleted) {
+          res.status(200).json({ message: 'User deleted!' });
+        } else {
+          res.status(404).json({ message: 'Could not find user with given id' });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: `Failed to delete user because ${err.message}.` });
+      });
+
+})
   
 
 
--- database/ailments/ailments-router.js
+++ database/ailments/ailments-router.js
@@ -3,5 +3,17 @@ const router = require('express').Router();
 const Ailments = require("./ailments-model");
 
 
+router.post('/', (req, res) => {
+
+    Ailments.add(req.body)
+        .then(ailments => {
+            res.status(201).json({symptoms: ailments})
+        })
+        .catch(error => {
+            res.status(500).json({message: error.message})
+        })
+
+})
+
 
 module.exports = router;
