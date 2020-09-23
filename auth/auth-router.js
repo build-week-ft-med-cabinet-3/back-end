864892255744559196f2bb41558a61445e82301d
Gregory Wilson <gjwilson7390@gmail.com>
2020-09-23 14:33:42 -0500

    still workin on CORS error

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -71,14 +71,14 @@ router.post('/login', (req, res) => {
   }
 });
 
-router.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    next()
-})
+// router.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept'
+//     )
+//     next()
+// })
 
 
 router.put('/:id', (req, res) => {
