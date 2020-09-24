d251882cd1c34e21d4ef132a46a93cde3cf13583
Gregory Wilson <gjwilson7390@gmail.com>
2020-09-24 14:55:08 -0500

    trying to fix CORS error

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+
 server.use('/api/auth', authRouter);
 server.use('/api/savedstrains', authenticate, strainsRouter);
 
